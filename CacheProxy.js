fc583cde5a4b9aaf763e5074090b040c5759084f
changhao.xin <changhao.xin@BKJK.COM>
2018-01-04 20:39:06 +0800

    add proxy

---
+++ CacheProxy.js
@@ -0,0 +1,40 @@
+// 先实现具体的两个算法
+const mult = function() {
+	let a = 1;
+	for (let i = 0; i < arguments.length; i++) {
+		a *= arguments[i];
+	}
+	return a;
+};
+
+const plus = function() {
+	let a = 0;
+	for (let i = 0; i < arguments.length; i++) {
+		a += arguments[i];
+	}
+	return a;
+};
+
+// 创建缓存代理
+const createProxyFactory = function(fn) {
+	let cache = {}; // 保存计算的结果
+	// 使用闭包在内存中保留对cache的引用
+	return function() {
+		let args = Array.from(arguments).join(','); // 将所有参数转化为字符串作为缓存的 key
+		if (args in cache) {
+			return cache[args];
+		} else {
+			return cache[args] = fn.apply(this, arguments);
+		}
+	};
+};
+
+// 使用代理对象
+const proxyMult = createProxyFactory(mult);
+const proxyPlus = createProxyFactory(plus);
+console.log(proxyMult(1,2,3,4)); // 24
+console.log(proxyPlus(1,2,3,4)); // 10
+
+//
+// 说明：这里每次进行同类的计算时（乘法和加法两类），先判断缓存对象cache中是否存在该参数连接成的字符串作为key的属性。
+// 如果有，则直接从cache中读取，否则就进行计算并保存其结果
---
+++ VirtualProxy.js
@@ -0,0 +1,36 @@
+// 虚拟代理：某一个花销很大的操作，可以通过虚拟代理的方式延迟到这种需要它的时候才去创建
+// 例：使用虚拟代理实现图片懒加载
+
+
+// 本体对象
+const imgFunc = (function() {
+	const imgNode = document.createElement('img');
+	document.body.appendChild(imgNode);
+	return {
+		setSrc(src){
+			imgNode.src = src;
+		}
+	}
+})();
+// 代理对象
+const proxyImage = (function() {
+	const img = new Image();
+	img.onload = function() {
+		imgFunc.setSrc(this.src);
+	};
+	return {
+		setSrc(src){
+			imgFunc.setSrc('./loading.gif');
+			img.src = src;
+		}
+	};
+})();
+// 使用代理对象
+proxyImage.setSrc('./real.png');
+
+//
+// 说明：图片懒加载的方式：先通过一张loading图占位，然后通过异步的方式加载图片，等图片加载好了再把完成的图片加载到img标签里面。
+// 这里讲述一下代理对象做了那些事：
+//     1.创建了一个 Image 对象，并为其绑定了 onload 事件。
+//     2.将 imgNode 先设置为 './loading.gif' 加载的菊花图。
+//     3.当 Image 对象加载完真实的图片，也就是上文的 './reality.png' ,将 imgNode 设置为 './reality.png'。
